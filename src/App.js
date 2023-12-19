4c0dcab0e8fad2d5732a6f4bd09043fe366b7828
13rstuart7 <135680232+13rstuart7@users.noreply.github.com>
2023-12-18 17:14:41 -0700

    updated html

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 // src/App.js
 
 import './App.css';
-import Header from "./components/Header"
-import Projects from "./components/Projects"
-import About from "./components/About"
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
+import Header from "../components/Header"
+import Projects from "../components/Projects"
+import About from "../components/About"
+import Contact from "../components/Contact";
+import Footer from "../components/Footer";
 
 function App() {
   return (
