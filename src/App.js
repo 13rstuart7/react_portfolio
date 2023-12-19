f2c1aef1c911036e9dcc6d8556e6719b7c176b23
13rstuart7 <135680232+13rstuart7@users.noreply.github.com>
2023-12-18 17:41:10 -0700

    updated src folder

--- src/App.js
+++ src/App.js
@@ -1,40 +1,42 @@
-// src/App.js
-
-import './App.css';
-import Header from "../components/Header"
-import Projects from "../components/Projects"
-import About from "../components/About"
-import Contact from "../components/Contact";
-import Footer from "../components/Footer";
+// import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+// import MainPage from "./pages";
+import Nav from "./components/Navigation";
+import AboutMe from "../src/pages/AboutMe";
+import Contact from "../src/pages/Contact";
+import React, { useState } from "react";
+import PortfolioContainer from "./components/PortfolioContainer";
 
 function App() {
+  const [pages] = useState([
+    {
+      name: "about me",
+    },
+    { name: "portfolio" },
+    { name: "contact" },
+    {
+      name: "resume",
+    },
+  ]);
+  const [currentPage, setCurrentPage] = useState(pages[0]);
+
+  // setting up main page
   return (
     <div className="App">
-<div>
-<section>
-        <Header />
-      </section>
-      
-      <section>
-        <About />
-      </section>
-      
-      <section>
-        <Projects />
-      </section>
-      
-      <section>
-        <Contact />
-      </section>
-
-      <section>
-        <Footer />
-      </section>
-
-</div>
-
+      <Nav
+        className="App-nav"
+        setCurrentPage={setCurrentPage}
+        currentPage={currentPage}
+        pages={pages}
+      />
+      <Header className="App-header" />
+      <PortfolioContainer currentPage={currentPage} />
+      <Footer className="App-footer" />
     </div>
   );
 }
 
+//exporting app
 export default App;
