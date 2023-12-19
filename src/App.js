feaedcdc61572b1db93748faaaffe892c5a61b28
13rstuart7 <135680232+13rstuart7@users.noreply.github.com>
2023-12-18 22:46:05 -0700

    fixed appjs

--- src/App.js
+++ src/App.js
@@ -1,41 +1,47 @@
 // import logo from "./logo.svg";
+
 import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-// import MainPage from "./pages";
 import Nav from "./components/Navigation";
-import AboutMe from "../src/pages/AboutMe";
-import Contact from "../src/pages/Contact";
+import AboutMe from "./pages/AboutMe";
+import Contact from "./pages/Contact";
 import React, { useState } from "react";
 import PortfolioContainer from "./components/PortfolioContainer";
 
-function App() {
-  const [pages] = useState([
-    {
-      name: "about me",
-    },
-    { name: "portfolio" },
-    { name: "contact" },
-    {
-      name: "resume",
-    },
-  ]);
-  const [currentPage, setCurrentPage] = useState(pages[0]);
 
-  // setting up main page
-  return (
-    <div className="App">
-      <Nav
-        className="App-nav"
-        setCurrentPage={setCurrentPage}
-        currentPage={currentPage}
-        pages={pages}
-      />
-      <Header className="App-header" />
-      <PortfolioContainer currentPage={currentPage} />
-      <Footer className="App-footer" />
-    </div>
-  );
+function App() {
+  // State for managing pages in the app
+const [pages] = useState([
+  { name: "about me" },
+  { name: "portfolio" },
+  { name: "contact" },
+  { name: "resume" },
+]);
+
+// State for tracking the currently active page
+const [currentPage, setCurrentPage] = useState(pages[0]);
+
+
+  // Setting up the main page structure
+return (
+  <div className="App">
+    {/* Navigation component */}
+    <Nav
+      className="App-nav"
+      setCurrentPage={setCurrentPage}
+      currentPage={currentPage}
+      pages={pages}
+    />
+    {/* Header component */}
+    <Header className="App-header" />
+    {/* PortfolioContainer component */}
+    <PortfolioContainer currentPage={currentPage} />
+    {/* Footer component */}
+    <Footer className="App-footer" />
+  </div>
+);
+
 }
 
 //exporting app
